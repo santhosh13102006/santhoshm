996f32a89dcc38ffa5dac2ceef1dc3f3661f56b2
santhosh13102006 <santhoshmohan13102006@gmail.com>
2026-01-20 22:12:30 +0530

    Add files via upload

---
+++ removeduplicate.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int removeDuplicates(int nums[], int n) {
+    if (n == 0)
+        return 0;
+
+    int k = 1;  // index for unique elements
+
+    for (int i = 1; i < n; i++) {
+        if (nums[i] != nums[i - 1]) {
+            nums[k] = nums[i];
+            k++;
+        }
+    }
+    return k;  // number of unique elements
+}
+
+int main() {
+   int n;
+   printf("enter number of elements in array:");
+   scanf("%d",&n);
+   int nums[n];
+   printf("enter %d elements:\n",n);
+   for(int i=0;i<n;i++){
+   scanf("%d",&nums[i]);}
+
+    int k = removeDuplicates(nums, n);
+
+    printf("Unique elements:\n");
+    for (int i = 0; i < k; i++) {
+        printf("%d ", nums[i]);
+    }
+
+    return 0;
+}
